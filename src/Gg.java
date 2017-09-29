4b1d9250eee5a3bddbc0aa2fb01bf4a6f642ca93
Supanut <zoleheartzzh@gmail.com>
2017-09-30 03:43:06 +0700

    improve code

--- src/Gg.java
+++ src/Gg.java
@@ -1,13 +1,20 @@
+/*
+ /**
+ * User interface for a menu and ordering system,
+ * using console interface.
+ *
+ * @author Supanut Petchnapaphan
+ */
 import java.util.Scanner;
 
 public class Gg {
 
-    static double p, c, ck, ca, ms = 0;
-    static int pp, cp, ckp, m, t, q, pq, cq, ckq, cap, caq, msq, msp, k1, k2, k3, k4, k5;
+    static double pizza, chicken, coke, cake, milkshake = 0;
+    static int pizzapiece, chickenpiece, cokepiece, menu,quantity, pizzaquantity, chickenquantity, cokequantity, cakepiece, cakequantity, milkshakequantity, milkshakepiece, collector1, collector2, collector3, collector4, collector5;
     static String surname, lastname;
     static Scanner k = new Scanner(System.in);
 
-    public static void greeter(String surname, String lastname) {
+    public static void printGreeter(String surname, String lastname) {
         Scanner n = new Scanner(System.in);
         System.out.print("Enter your surname: ");
         surname = n.next();
@@ -18,7 +25,7 @@ public class Gg {
 
     }
 
-    public static void first() {
+    public static void printOrder() {
         System.out.println("--------- Welcome to SKE Restaurant ---------");
         System.out.println("1.) Pizza 250 Baht.");
         System.out.println("2.) Chickens 120 Baht.");
@@ -29,89 +36,97 @@ public class Gg {
         System.out.println("7.) Exit");
     }
 
-    public static double food(int m, int q) {
-        if (m == 1) {
-            p++;
-            pq = q;
-            pp = pq * 250;
-            k1 = 1;
-        } else if (m == 2) {
-            c++;
-            cq = q;
-            cp = cq * 120;
-            k2 = 1;
-        } else if (m == 3) {
-            ck++;
-            ckq = q;
-            ckp = ckq * 45;
-            k3 = 1;
-        } else if (m == 4) {
-            ca++;
-            caq = q;
-            cap = caq * 80;
-            k4 = 1;
-        } else if (m == 5) {
-            ms++;
-            msq = q;
-            msp = msq * 60;
-            k5 = 1;
+    public static double addOrder(int menu, int quantity) {
+        switch ( menu ) {
+            case 1:
+                pizza++;
+                pizzaquantity = quantity;
+                pizzapiece = pizzaquantity * 250;
+                collector1 = 1;
+                break;
+            case 2:
+                chicken++;
+                chickenpiece = quantity;
+                chickenpiece = chickenquantity * 120;
+                collector2 = 1;
+                break;
+            case 3:
+                coke++;
+                cokequantity = quantity;
+                cokepiece = cokequantity * 45;
+                collector3 = 1;
+                break;
+            case 4:
+                cake++;
+                cakequantity = quantity;
+                cakepiece = cakequantity * 80;
+                collector4 = 1;
+                break;
+            case 5:
+                milkshake++;
+                milkshakequantity = quantity;
+                milkshakepiece = milkshakequantity * 60;
+                collector5 = 1;
+                break;
+            default:
+                break;
         }
-        return k1 | k2 | k3 | k4 | k5;
+        return collector1 | collector2 | collector3 | collector4 | collector5;
     }
 
-    public static void bill(int m, double p, double c, double ck, double ca, double ms) {
-        double sum = 0;
-        sum = pp + cp + ckp + cap + msp;
-        if (m == 6) {
+    public static void printBill(int menu, double pizza, double chicken, double coke, double cake, double milkshake) {
+        double total = 0;
+        total = pizzapiece + chickenpiece + cokepiece + cakepiece + milkshakepiece;
+        if ( menu == 6 ) {
             System.out.println("o------ Menu ------+-- Qty --+-- Price --o");
-            if (p > 0) {
-                System.out.printf("| Pizza            |  %5d  |    %5d  |\n", pq, pp);
+            if ( pizza > 0 ) {
+                System.out.printf("| Pizza            |  %5d  |    %5d  |\n", pizzaquantity, pizzapiece);
             }
-            if (c > 0) {
-                System.out.printf("| Chikens          |  %5d  |    %5d  |\n", cq, cp);
+            if ( chicken > 0 ) {
+                System.out.printf("| Chikens          |  %5d  |    %5d  |\n", chickenquantity, chickenpiece);
             }
-            if (ck > 0) {
-                System.out.printf("| Coke             |  %5d  |    %5d  |\n", ckq, ckp);
+            if ( coke > 0 ) {
+                System.out.printf("| Coke             |  %5d  |    %5d  |\n", cokequantity, cokepiece);
             }
-            if (ca > 0) {
-                System.out.printf("| Cake             |  %5d  |    %5d  |\n", caq, cap);
+            if ( cake > 0 ) {
+                System.out.printf("| Cake             |  %5d  |    %5d  |\n", cakequantity, cakepiece);
             }
-            if (ms > 0) {
-                System.out.printf("| Milkshake        |  %5d  |    %5d  |\n", msq, msp);
+            if ( milkshake > 0 ) {
+                System.out.printf("| Milkshake        |  %5d  |    %5d  |\n", milkshakequantity, milkshakepiece);
             }
             System.out.println("o------------------o---------o-----------o");
-            System.out.printf("| Total                      |    %5f  |\n", (sum));
-            System.out.printf("| Vat 7 percent              |    %5f  |\n", (sum * 1.07));
+            System.out.printf("| Total                      |    %.0f    |\n", (total));
+            System.out.printf("| Vat 7 percent              |    %.2f |\n", (total * 1.07));
             System.out.println("o------------------o---------o-----------o");
             System.out.println("");
         }
     }
-    public static void re () {
+
+    public static void repeatOrder() {
         do {
             System.out.print("Enter your Choice: ");
-            m = k.nextInt();
-            bill(m, p, c, ckp, ca, ms);
-            if (m == 7) {
+            menu = k.nextInt();
+            printBill(menu, pizza, chicken, coke, cake, milkshake);
+            if ( menu == 7 ) {
                 break;
             }
-            if (m != 6) {
+            if ( menu != 6 ) {
                 System.out.print("Enter Quantity: ");
-                q = k.nextInt();
+                quantity = k.nextInt();
                 System.out.println("");
-                food(m, q);
+                addOrder(menu, quantity);
             }
-        } while (m != 7);
+        } while ( menu != 7 );
         System.out.println("===== Thank you =====");
         System.out.print("Have a nice day :D");
         System.out.println();
     }
 
     public static void main(String[] args) {
-        
-        greeter(surname, lastname);
-        first();
-        re();
+
+        printGreeter(surname, lastname);
+        printOrder();
+        repeatOrder();
 
     }
 }
-
